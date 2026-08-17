6482695102880f004f882d8933900cfe3e01d6c6
Ujjwal Tyagi <ujjwaltyagi9700@gmail.com>
2026-08-18 01:18:31 +0530

    Add CI/CD pipeline using GitHub Actions

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -5,7 +5,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.time.Duration;
 
@@ -21,12 +23,33 @@ public class DriverManager {
 
             WebDriver newDriver;
 
+            // Check if running in CI environment (GitHub Actions sets this automatically)
+            boolean isCI = System.getenv("CI") != null;
+
             if (browserName.equalsIgnoreCase("chrome")) {
                 WebDriverManager.chromedriver().setup();
-                newDriver = new ChromeDriver();
+
+                ChromeOptions options = new ChromeOptions();
+                if (isCI) {
+                    // No visible display exists on CI runners, so Chrome
+                    // must run headless - without rendering an actual window
+                    options.addArguments("--headless=new");
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
+                    options.addArguments("--window-size=1920,1080");
+                    logger.info("Running in CI environment - Chrome headless mode enabled.");
+                }
+                newDriver = new ChromeDriver(options);
+
             } else if (browserName.equalsIgnoreCase("firefox")) {
                 WebDriverManager.firefoxdriver().setup();
-                newDriver = new FirefoxDriver();
+
+                FirefoxOptions options = new FirefoxOptions();
+                if (isCI) {
+                    options.addArguments("--headless");
+                }
+                newDriver = new FirefoxDriver(options);
+
             } else {
                 logger.error("Browser not supported: " + browserName);
                 throw new RuntimeException("Browser not supported: " + browserName);
